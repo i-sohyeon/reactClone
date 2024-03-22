82b6dcfa478538df134a0ecd7a9278693eb18e56
sohyeon <sohyun9743@naver.com>
2024-03-22 16:34:19 +0900

    useRouteMatch, style 적용

--- coin-app/src/routes/Coin.tsx
+++ coin-app/src/routes/Coin.tsx
@@ -3,6 +3,7 @@ import { Switch, Route, useParams, useLocation } from "react-router";
 import styled from "styled-components";
 import Price from "./Price";
 import Chart from "./Chart";
+import { Link, useRouteMatch } from "react-router-dom";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -43,6 +44,28 @@ const OverviewItem = styled.div`
 `
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
 `;
 
 interface RouteParams {
@@ -125,6 +148,10 @@ function Coin() {
   const {state} = useLocation<RouteState>();
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useRouteMatch("/:coinId/price");
+  const chartMatch = useRouteMatch("/:coinId/chart");
+  console.log(priceMatch);
+
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -180,6 +207,16 @@ function Coin() {
           <span>{priceInfo?.max_supply}</span>
         </OverviewItem>
       </Overview>
+
+      <Tabs>
+        <Tab isActive={chartMatch !== null }>
+          <Link to={`/${coinId}/chart`}>Chart</Link>
+        </Tab>
+        <Tab isActive={priceMatch !== null }>
+          <Link to={`/${coinId}/price`}>Price</Link>
+        </Tab>
+      </Tabs>
+
       <Switch>
         <Route path={`/${coinId}/price`}>
           <Price />
