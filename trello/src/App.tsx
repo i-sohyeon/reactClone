3dc8a7f178af9cfae99e875e318395a64731815f
sohyeon <sohyun9743@naver.com>
2024-04-09 17:49:24 +0900

    DragDrop style, component

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,22 +1,65 @@
 import React from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0px auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(3, 1fr);
+`
+
+const Board =  styled.div`
+  padding: 30px 10px;
+  border-radius: 5px;
+  background: ${(props) => props.theme.boardColor};
+  min-height: 180px;
+`
+const Card =  styled.div`
+  border-radius: 5px;
+  margin-bottom: 10px;
+  background: ${(props) => props.theme.cardColor};
+`
+const toDos = ["a","b","c","d","e","f","g",]
 
 function App() {
   const onDragEnd = () => {}
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-      <Droppable droppableId='one'>
-         {() => <ul>
-            <Draggable draggableId='first' index={0}>
-              {() => <li>One</li>}
-            </Draggable>
-            <Draggable draggableId='second' index={1}>
-              {() => <li>Two</li>}
-            </Draggable>
-          </ul>}
-      </Droppable>
-      </div>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId='one'>
+            {(magic) => (
+              <Board ref={magic.innerRef } {...magic.droppableProps}>
+                {toDos.map((todo, index) => <Draggable draggableId={todo} index={index}>
+                  {(magic) => (
+                    <Card 
+                      ref={magic.innerRef}
+                      {...magic.draggableProps}
+                      {...magic.dragHandleProps}
+                    >
+                      <span {...magic.dragHandleProps}>⭐</span>
+                      {todo}
+                    </Card>
+                  )}
+                </Draggable>)
+                }
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+          
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 }
